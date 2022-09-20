a69d7b87fd05056a0068a5433be3175b4ee8bd59
ElMarto <martin@mata.com>
2022-09-20 14:06:11 -0300

    Agrega estilos a pantalla de busqueda

--- src/views/SearchBoxView.tsx
+++ src/views/SearchBoxView.tsx
@@ -1,8 +1,23 @@
+import './searchBoxView.css';
+
 export const SearchBoxView = () => {
     return (
-        <div style={{backgroundColor: '#ffe600', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <img src="assets/Logo_ML.png" alt="Mercadolibre logo" /> 
-            <input type="text" placeholder="Nunca dejes de buscar" />
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-sm-12 contenedorPrincipal">
+                    <div className="col-sm-1"></div>
+                    <div className="col-sm-1">
+                        <img src="assets/Logo_ML.png" alt="Mercadolibre logo" />
+                    </div>
+                    <div className="col-sm-9 contenedorCajaBusqueda">
+                        <input type="text" placeholder="Nunca dejes de buscar" style={{width: '100%'}} />
+                        <div className="botonBuscar">
+                            <img src="assets/ic_Search.png" alt="Buscar" />
+                        </div>
+                    </div>
+                    <div className="col-sm-1"></div>
+                </div>
+            </div>
         </div>
     );
 }
