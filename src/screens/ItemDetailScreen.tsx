2e33726275aa49c5f8fb095cd45d6621683cb8b6
ElMarto <martin@mata.com>
2022-09-24 00:53:35 -0300

    Consulta detalle de item en pantalla detalle

--- src/screens/ItemDetailScreen.tsx
+++ src/screens/ItemDetailScreen.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useItems } from "../hooks/useItems";
+import { SingleItem } from "../types/SingleItem";
+import { SingleItemResponse } from "../types/SingleItemResponse";
+import { ItemDetailView } from "../views/ItemDetailView";
 
 export const ItemDetailScreen = () => {
 
     const { id } = useParams();
+    const { getItemDetail, loadingItems} = useItems();
+    const [item, setItem] = useState<SingleItem>();
 
-    return (
-        <span>Detalle de item {id}</span>
-    );
+    useEffect(() => {
+        const loadingData = async () => {
+            const data:SingleItemResponse = await getItemDetail(id || '');
+            setItem(data.item);
+        };
+
+        loadingData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    if (item) {
+        return (
+            <ItemDetailView item={item} />
+        );
+    } else {
+        return <span>Error en consulta de item</span>
+    }
 };
