e0589451e471fdf646c94efcb54b71cbdf25c2a3
ElMarto <martin@mata.com>
2022-09-27 22:06:14 -0300

    Corrige url de logo de ML e icono de lupa para que sean visibles tambien en pantalla de detalle de item

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -12,12 +12,12 @@ export const SearchBox = ({onClickSearch, onSearchProductChange}: SearchBoxProps
                 <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12 contenedorPrincipal">
                     <div className="col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
                     <div className="col-sm-1 col-md-1 col-lg-1 col-xl-1" style={{display: 'flex', flexDirection: 'row-reverse', marginRight: 10}}>
-                        <img src="assets/Logo_ML.png" alt="Mercadolibre logo" />
+                        <img src="/assets/Logo_ML.png" alt="Mercadolibre logo" />
                     </div>
                     <div className="col-sm-9 col-md-9 col-lg-9 col-xl-9 contenedorCajaBusqueda">
                         <input type="text" placeholder="Nunca dejes de buscar" style={{width: '100%'}} onChange={onSearchProductChange} />
                         <div className="botonBuscar">
-                            <img src="assets/ic_Search.png" alt="Buscar" onClick={onClickSearch} />
+                            <img src="/assets/ic_Search.png" alt="Buscar" onClick={onClickSearch} />
                         </div>
                     </div>
                     <div className="col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
