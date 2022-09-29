90acd429ac8fe9797029dfa8286fa075f3f3dcb8
ElMarto <martin@mata.com>
2022-09-28 23:10:06 -0300

    Remueve bootstrap de CategoryWrapper

--- src/components/CategoryWrapper.tsx
+++ src/components/CategoryWrapper.tsx
@@ -1,5 +1,3 @@
-import './CategoryWrapper.css';
-
 interface CategoryWrapperProps {
     children: React.ReactNode;
     breadcrumb?: string;
@@ -7,25 +5,13 @@ interface CategoryWrapperProps {
 
 export const CategoryWrapper = ({children, breadcrumb}: CategoryWrapperProps) => {
     return (
-        <>
-            <div className="row">
-                <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12" style={{margin: 16, display: 'flex', flexDirection: 'row'}}>
-                    <div className="col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
-                    <div className="col-sm-10 col-md-10 col-lg-10 col-xl-10">
-                        <span className="categoryText">{breadcrumb}</span>
-                    </div>
-                    <div className="col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+        <div className="categoryWrapperContainer">
+            <div className="categoryWrapperBreadcrumbContainer">
+                <span className="breadcrumbText">{breadcrumb}</span>
+                <div>
+                    {children}
                 </div>
             </div>
-            <div className="row">
-                <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12" style={{margin: 16}}>
-                    <div className="col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
-                    <div className="col-sm-10 col-md-10 col-lg-10 col-xl-10">
-                        {children}
-                    </div>
-                    <div className="col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
-                </div>
-            </div>
-        </>
+        </div>
     );
 }
