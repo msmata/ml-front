628d9a6f24729d7d061f101f8ec718ad9a554a7b
Martin Mata <martin.mata@grupoesfera.com.ar>
2022-09-30 00:10:19 -0300

    Permite hacer busquedas pulsando Enter

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -7,13 +7,28 @@ interface SearchBoxProps {
 }
 
 export const SearchBox = ({searchProduct, onClickSearch, onSearchProductChange}: SearchBoxProps) => {
+
+    const handleKeyDown = (e: any) => {
+        if (e.key === 'Enter') {
+            onClickSearch();
+        }
+    }
+
     return (
         <div className="searchboxContainer">
             <div className="searchboxContentContainer">
                 <div className="logoContainer">
                     <img src="/assets/Logo_ML.png" alt="Mercadolibre logo" />
                 </div>
-                <input type="text" placeholder="Nunca dejes de buscar" style={{width: '100%'}} onChange={onSearchProductChange} value={searchProduct} />
+                <input
+                    type="text"
+                    placeholder="Nunca dejes de buscar"
+                    style={{width: '100%'}}
+                    onChange={onSearchProductChange}
+                    value={searchProduct}
+                    // onKeyDown={e => e.key === 'Enter' && onClickSearch}
+                    onKeyDown={handleKeyDown}
+                />
                 <button onClick={onClickSearch} className="searchButton">
                     <img src="/assets/ic_Search.png" alt="Buscar" />
                 </button>
